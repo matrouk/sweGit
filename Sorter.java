8811cb4cbb8db0a5041438bee954e73a87cc85b0
Hussain Almatrouk <hmm5522@hotmail.com>
2023-02-13 00:20:57 +0300

    selectionSort

--- Sorter.java
+++ Sorter.java
@@ -1,5 +1,29 @@
 public class Sorter{
     public static void main(String[] args) {
-        System.out.println("hello");
+        int[] array = {4,1,2,7,8,9,10};
+        selectionSort(array);
+        printarrayay(array);
+
+    }
+
+    public static void selectionSort(int array[]) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_element = i;
+            for (int j = i + 1; j < n; j++)
+                if (array[j] < array[min_element])
+                    min_element = j;
+            int temp = array[min_element];
+            array[min_element] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    // Method to print the elements of an array
+    public static void printarrayay(int array[]) {
+        int n = array.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(array[i] + " ");
+        System.out.println();
     }
 }
