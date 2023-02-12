231fd94e9d34437d1d00e7fc50921e31c68bbba7
Feras532 <saf_523_@hotmail.com>
2023-02-13 01:26:29 +0300

    sort method

--- Sorter.java
+++ Sorter.java
@@ -5,6 +5,9 @@ public class Sorter{
         printarrayay(array);
 
     }
+    public static void sort(int[] array){
+        sortInsertion(array);
+    }
 
     public static void selectionSort(int array[]) {
         int n = array.length;
@@ -16,6 +19,23 @@ public class Sorter{
             int temp = array[min_element];
             array[min_element] = array[i];
             array[i] = temp;
+        }
+    }
+
+    public static void sortInsertion(int [] sort_arr){
+
+        for(int i=0;i<sort_arr.length;++i){
+
+          int j = i;
+
+          while(j > 0 && sort_arr[j-1]>sort_arr[j]){
+
+            int key = sort_arr[j];
+            sort_arr[j] = sort_arr[j-1];
+            sort_arr[j-1] = key;
+            j = j-1; 
+
+          }
         }
     }
 
