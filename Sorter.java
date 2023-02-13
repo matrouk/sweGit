b4876213e2e72702a107b2f4956d55920211c977
Hussain Almatrouk <hmm5522@hotmail.com>
2023-02-13 15:27:57 +0300

    "last"

--- Sorter.java
+++ Sorter.java
@@ -25,6 +25,23 @@ public class Sorter{
         }
     }
 
+    public static void sortInsertion(int [] sort_arr){
+
+        for(int i=0;i<sort_arr.length;++i){
+
+          int j = i;
+
+          while(j > 0 && sort_arr[j-1]>sort_arr[j]){
+
+            int key = sort_arr[j];
+            sort_arr[j] = sort_arr[j-1];
+            sort_arr[j-1] = key;
+            j = j-1; 
+
+          }
+        }
+    }
+
     // Method to print the elements of an array
     public static void printarrayay(int array[]) {
         int n = array.length;
